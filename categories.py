2c9eadadb99f4546d6bd9bb1aedd8bcbc1956dfa
Thibaud Mars <thibaudmars@hotmail.fr>
2022-07-13 17:34:21 +0200

    Scraping pour un page de catégorie. Reste 1 erreur lors de la création du fichier CSV a traiter.

---
+++ categories.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+from products import get_product_information
+
+main_url = 'http://books.toscrape.com'
+categories_path = '/catalogue/category/books'
+products_path = '/catalogue'
+category_page_url = main_url + categories_path + '/add-a-comment_18/index.html'
+
+
+def find_number_of_pages(number_of_products):
+    if number_of_products - round(number_of_products/20, 0) >= 0.5:
+        number_of_pages = int(round(number_of_products/20,0) + 1)
+    else:
+        number_of_pages = int(round(number_of_products/20,0))
+    return str(number_of_pages)
+
+
+def format_product_url(product_url):
+    product_page_url = main_url + products_path + product_url.replace('../../..','')
+    return product_page_url
+
+
+def save_products_information_in_csv(product_information_data):
+    with open('products.csv', 'w' ,newline='') as file:
+        headers = ['product_page_url', 'upc', 'title', 'price_including_tax', 'price_excluding_tax', 'number_available',
+                   'product_description', 'category', 'review_rating', 'image_url']
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(headers)
+        writer.writerows(product_information_data)
+        #for product in product_information_data:
+        #print(product)
+
+
+
+response = requests.get(category_page_url)
+if response.ok:
+    products_information_data = []
+    soup = BeautifulSoup(response.text, 'lxml')
+    number_of_products = soup.find('form').findNext('strong').text
+    number_of_pages = find_number_of_pages(int(number_of_products))
+    products_in_page = soup.find('ol')
+    for product in products_in_page.findAll('h3'):
+        product_url = format_product_url(product.find('a')['href'])
+        products_information_data.append(get_product_information(product_url))
+    save_products_information_in_csv(products_information_data)
--- products.py
+++ products.py
@@ -1,10 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 main_url = 'http://books.toscrape.com'
-product_page_url = main_url + '/catalogue/sharp-objects_997/index.html'
-
-response = requests.get(product_page_url)
+product_page_url = main_url + '/catalogue/tipping-the-velvet_999/index.html'
 
 
 def convert_star_rating(star_rating):
@@ -31,6 +30,8 @@ def format_data(string_data):
         data = data.replace('Â£', '£')
     elif 'â\x80\x94' in data:
         data = data.replace('â\x80\x94', '—')
+    elif 'â' in data:
+        data = data.replace('â', '—')
     return data
 
 
@@ -39,34 +40,28 @@ def format_number_available(raw_number_available):
     number_available = number_available.replace(' available)', '')
     return number_available
 
-
-def save_product_information_in_csv(product_information_data):
-    with open('product.csv', 'w') as file:
-        file.write("URL du produit,Code UPC,Titre,Prix TTC,Prix HT,Stock,Desctiption,Catégorie,Note,URL de l'image\n")
-        file.write(product_information_data[0] + ',' + product_information_data[1] + ',' + product_information_data[2] +
-                    ',' + product_information_data[3] + ',' + product_information_data[4] + ',' +
-                    product_information_data[5]  + ',' + product_information_data[7] +
-                    ',' + product_information_data[8] + ',' + product_information_data[9] + '\n')
+def format_category(raw_category):
+    category = raw_category.replace('\n','')
+    return category
 
 
-if response.ok:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    product_information = soup.find('table', 'table-striped',)
-    upc = product_information.find(string='UPC').findParent('tr').findChild('td').text
-    price_including_tax = format_data(product_information.find(string='Price (incl. tax)').findParent('tr').findChild('td').text)
-    price_excluding_tax = format_data(product_information.find(string='Price (excl. tax)').findParent('tr').findChild('td').text)
-    image_url = soup.find('img').attrs['src'].replace('../..', main_url)
-    # A revoir
-    #product_description = format_data(soup.find(string='Product Description').findNext('p').text)
-    #
-    number_available = format_number_available(product_information.find(string='Availability').findParent('tr').findChild('td').text)
-    category = soup.find('ul', 'breadcrumb').find(string='Books').findNext('li').text
-    title = soup.find('h1').text
-    star_rating = soup.find('p', 'star-rating').attrs['class']
-    review_rating = convert_star_rating(star_rating)
-    product_information_data = [product_page_url, upc, title, price_including_tax, price_excluding_tax,
-                                number_available, product_description, category, review_rating, image_url]
-    print(product_information_data)
-    save_product_information_in_csv(product_information_data)
+def get_product_information(product_page_url):
+    response = requests.get(product_page_url)
+    if response.ok:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        product_information = soup.find('table', 'table-striped',)
+        upc = product_information.find(string='UPC').findParent('tr').findChild('td').text
+        price_including_tax = format_data(product_information.find(string='Price (incl. tax)').findParent('tr').findChild('td').text)
+        price_excluding_tax = format_data(product_information.find(string='Price (excl. tax)').findParent('tr').findChild('td').text)
+        image_url = soup.find('img').attrs['src'].replace('../..', main_url)
+        product_description = format_data(soup.find(string='Product Description').findNext('p').text)
+        number_available = format_number_available(product_information.find(string='Availability').findParent('tr').findChild('td').text)
+        category = format_category(soup.find('ul', 'breadcrumb').find(string='Books').findNext('li').text)
+        title = soup.find('h1').text
+        star_rating = soup.find('p', 'star-rating').attrs['class']
+        review_rating = convert_star_rating(star_rating)
+        product_information_data = [product_page_url, upc, title, price_including_tax, price_excluding_tax,
+                                    number_available, product_description, category, review_rating, image_url]
+        return product_information_data
 
 
